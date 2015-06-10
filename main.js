7174541cdc3b28eee6d7c24e2b77979b715020fe
Kotaru, Venkata (DS) <Venkata.Kotaru@adp.com>
2015-06-10 19:21:00 +0530

    Routes added.

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 
 angular.module('VenCKi-Angular-Material', ["ngMaterial", "ui.router", "ngMdIcons"])
     .config(function($stateProvider, $urlRouterProvider, $mdThemingProvider){
-        $stateProvider.state('home', {
-            url:'/home',
-            controller: require('./src/js/home'),
-            templateUrl: 'src/templates/home.html'
-        });
+        $stateProvider
+            .state('home', {
+                url:'/home',
+                controller: require('./src/js/home'),
+                templateUrl: 'src/templates/home.html'
+            })
+            .state('home.recipeList', {
+                url:'/recipes',
+                templateUrl:'src/templates/recipeList.html'
+            })
+            .state('home.techStack', {
+                url:'/techStack',
+                templateUrl:'src/templates/techStack.html'
+            })
+            .state('home.about', {
+                url:'/about',
+                templateUrl:'src/templates/about.html'
+            });
+
+/*        $mdThemingProvider.theme('default')
+            .backgroundPalette('lime');*/
 
         $mdThemingProvider.theme('custom')
             .primaryPalette('pink')
             .accentPalette('orange')
             .backgroundPalette('light-blue');
 
-        $urlRouterProvider.otherwise('/home');
+        $urlRouterProvider.otherwise('/home/recipes');
     })
 ;
