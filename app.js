1e44e1bf9acf5f2d8ec837ce128473da4bedba88
VenCKi <kvkirthy@gmail.com>
2015-06-20 01:32:55 +0530

    more for elements added

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ module.exports = function($scope, $mdToast){
     $scope.showToast = function(msg){
         $mdToast.show($mdToast.simple()
                 .content(msg)
-                .position('top right')
+                .position('bottom right')
         );
     };
 }
--- src/js/bottomSheetController.js
+++ src/js/bottomSheetController.js
@@ -2,7 +2,7 @@ module.exports = function($scope, $mdToast){
     $scope.showToast = function(msg){
         $mdToast.show($mdToast.simple()
                 .content(msg)
-                .position('top right')
+                .position('bottom right')
         );
     };
 }
